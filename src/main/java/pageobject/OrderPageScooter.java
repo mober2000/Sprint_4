072331833694128e858c0425d961e8fbed84017c
a.chebanov <a.chebanov@evotor.ru>
2022-11-05 14:35:16 +0300

    My first fix tabulation and CodeStyle

--- src/main/java/pageobject/OrderPageScooter.java
+++ src/main/java/pageobject/OrderPageScooter.java
@@ -1,16 +1,12 @@
-package PageObject;
+package pageobject;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
-
-import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 
 public class OrderPageScooter {
-
     private WebDriver driver;
-
     //Локаторы  данных о клиенте
     private By orderFirstName = By.xpath(".//input[@placeholder='* Имя']");
     private By orderSecondName = By.xpath(".//input[@placeholder='* Фамилия']");
@@ -20,7 +16,6 @@ public class OrderPageScooter {
     private By orderMetroStation = By.className("Order_Text__2broi");
     private By orderContactPhone = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
     private By orderNextButton = By.xpath(".//button[text()='Далее']");
-
     //Локаторы данных об аренде
     private By orderWhen = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
     private By orderWhenCalendar = By.xpath(".//div[@aria-label='Choose четверг, 10-е ноября 2022 г.']");
@@ -29,92 +24,86 @@ public class OrderPageScooter {
     private By orderColorBlack = By.xpath(".//label[@for='black']");
     private By orderCommentCour = By.xpath(".//input[@placeholder='Комментарий для курьера']");
     private By orderButtonDone = By.xpath("//*[@class='Button_Button__ra12g Button_Middle__1CSJM' and contains(text(), 'Заказать')]");
-
-
     //Подтверждение заказа
     private By orderYesButton = By.xpath("//*[@class='Button_Button__ra12g Button_Middle__1CSJM' and contains(text(), 'Да')]");
     private By orderDoneHeader = By.xpath("//div[@class='Order_ModalHeader__3FDaJ and text()='Заказ оформлен']");
 
-    public OrderPageScooter(WebDriver driver){
+    public OrderPageScooter(WebDriver driver) {
         this.driver = driver;
     }
 
     //Ввод имени
-    public void sendFirstName(String name){
+    public void sendFirstName(String name) {
         driver.findElement(orderFirstName).sendKeys(name);
     }
 
     //Ввод фамилии
-    public void sendSecondName(String secondName){
+    public void sendSecondName(String secondName) {
         driver.findElement(orderSecondName).sendKeys(secondName);
     }
 
     //Ввод адреса
-    public void sendDeliveryAddress(String address){
+    public void sendDeliveryAddress(String address) {
         driver.findElement(orderAddressPoint).sendKeys(address);
     }
 
     //Выбор поля для ввода станции и выбора
-    public void sendMetroStationField(String metro){
+    public void sendMetroStationField(String metro) {
         driver.findElement(orderMetroStationField).sendKeys(metro);
         String metroName = driver.findElement(By.xpath("//div[@class='Order_Text__2broi' and text()='" + metro + "']")).getText();
         assertEquals(metroName, metro);
         driver.findElement(By.xpath("//div[@class='Order_Text__2broi' and text()='" + metro + "']")).click();
     }
 
-
     //Ввод телефона
-    public void sendNumberPhone(String number){
+    public void sendNumberPhone(String number) {
         driver.findElement(orderContactPhone).sendKeys(number);
     }
 
     //Клик по кнопке далее
-    public void clickNextWindow(){
+    public void clickNextWindow() {
         driver.findElement(orderNextButton).click();
     }
 
-
-
     //Клик на выбор даты
-    public void clickDateButton(){
+    public void clickDateButton() {
         driver.findElement(orderWhen).click();
     }
 
     //Клик на конкретную дату
-    public void clickSendDate(){
+    public void clickSendDate() {
         driver.findElement(orderWhenCalendar).click();
     }
+
     //Клик на выпадающий список выбора времени аренды
-    public void orderHowLong(){
+    public void orderHowLong() {
         driver.findElement(orderHowLong).click();
     }
+
     //Клик на выбор времени аренды
-    public void clickChangeTimeLeasing(){
+    public void clickChangeTimeLeasing() {
         driver.findElement(orderHowLongTwoDays).click();
     }
+
     //Выбор чекбокса с цветом
-    public void clickOrderColorBlack(){
+    public void clickOrderColorBlack() {
         driver.findElement(orderColorBlack).click();
     }
+
     //Комментарий
-    public void sendComment(String comment){
+    public void sendComment(String comment) {
         driver.findElement(orderCommentCour).sendKeys(comment);
     }
+
     //Клик по кнопкам и отображение окна об успешном отображении заказа
-    public void clickOrderButtonDone(){
+    public void clickOrderButtonDone() {
         driver.findElement(orderButtonDone).click();
         driver.findElement(orderYesButton).click();
         driver.findElement(orderDoneHeader).isDisplayed();
     }
 
-
-
     //Методы окна ввода данных пользователя в одном шаге
-    public void sendUserData(String name,
-                             String secondName,
-                             String address,
-                             String station,
-                             String number){
+    public void sendUserData(String name, String secondName, String address, String station, String number) {
         sendFirstName(name);
         sendSecondName(secondName);
         sendDeliveryAddress(address);
@@ -124,7 +113,7 @@ public class OrderPageScooter {
     }
 
     //Метод ввода данных о доставке в один шаг
-    public void sendDeliveryDataAndDoneOrder(String comment){
+    public void sendDeliveryDataAndDoneOrder(String comment) {
         clickDateButton();
         clickSendDate();
         orderHowLong();
@@ -133,13 +122,5 @@ public class OrderPageScooter {
         sendComment(comment);
         clickOrderButtonDone();
     }
-
-
-
-
-
-
-
-
 }
 
--- src/test/java/OrderScooter.java
+++ src/test/java/OrderScooter.java
@@ -1,5 +1,5 @@
-import PageObject.HomePageScooter;
-import PageObject.OrderPageScooter;
+import pageobject.HomePageScooter;
+import pageobject.OrderPageScooter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,13 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class OrderScooter {
-
     private WebDriver driver;
 
     @Before
     public void beforeTest() {
         // создали драйвер для браузера Chrome
-
         ChromeOptions options = new ChromeOptions();
         driver = new ChromeDriver(options);
         // перешли на страницу тестового приложения
@@ -28,40 +26,28 @@ public class OrderScooter {
     }
 
     @Test
-    public void orderScooterFirstOrderButton(){
+    public void orderScooterFirstOrderButton() {
 
         HomePageScooter homePageScooter = new HomePageScooter(driver);
         homePageScooter.clickCookeButton();
         homePageScooter.clickFirstOrderButtonTitle();
         OrderPageScooter orderPageScooter = new OrderPageScooter(driver);
         orderPageScooter.sendUserData(
-                "Алексей",
-                "Чебанов",
-                "Москва",
-                "Новокосино",
-                "+79197795816");
+                "Алексей", "Чебанов", "Москва", "Новокосино", "+79197795816");
         orderPageScooter.sendDeliveryDataAndDoneOrder("Просьба везти аккуратно");
-
-
     }
 
 
     //Отредачить
     @Test
-    public void orderScooterSecondOrderButton(){
+    public void orderScooterSecondOrderButton() {
         HomePageScooter homePageScooter = new HomePageScooter(driver);
         homePageScooter.clickCookeButton();
         homePageScooter.scrollOrderButton();
         homePageScooter.clickSecondOrderButtonMiddlePage();
         OrderPageScooter orderPageScooter = new OrderPageScooter(driver);
         orderPageScooter.sendUserData(
-                "Алексей",
-                "Чебанов",
-                "Москва",
-                "Черкизовская",
-                "+79197795816");
+                "Алексей", "Чебанов", "Москва", "Черкизовская", "+79197795816");
         orderPageScooter.sendDeliveryDataAndDoneOrder("Просьба везти аккуратно");
     }
-
-
 }
